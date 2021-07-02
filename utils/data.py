f9d201f7f22b61f61b568df963e694e6c7ee2b7a
tom_armstrong <thomasarmstrong98@gmail.com>
2021-07-02 23:31:38 +0100

    first look at data and fixing import

--- utils/data.py
+++ utils/data.py
@@ -7,8 +7,8 @@ from utils.setup import Config
 
 def get_trade_and_book_by_stock_and_time_id(stock_id: int, time_id: int = None,
                                             datatype: str = 'train') -> Tuple[pd.DataFrame, pd.DataFrame]:
-    book_example = pd.read_parquet(f'{Config.data_directory}book_{datatype}.parquet/stock_id={stock_id}')
-    trade_example = pd.read_parquet(f'{Config.data_directory}trade_{datatype}.parquet/stock_id={stock_id}')
+    book_example = pd.read_parquet( Config.data_directory / f"book_{datatype}.parquet" / f"stock_id={stock_id}")
+    trade_example = pd.read_parquet( Config.data_directory / f"trade_{datatype}.parquet" / f"stock_id={stock_id}")
     if time_id:
         book_example = book_example[book_example['time_id'] == time_id]
         trade_example = trade_example[trade_example['time_id'] == time_id]
